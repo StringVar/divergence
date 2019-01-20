fa1ec71450df760bde747997c1ab09107f6e2493
Cameron White <cameron.ag.white@gmail.com>
2019-01-19 18:17:12 -0700

    everything that I wanted to do is done.

--- divergence.js
+++ divergence.js
@@ -2,12 +2,13 @@
 var count = 0;
 var stop = false;
 var maxRadius = 650;
-var radiusInc= 10;
+var radiusInc = 10;
+var removeCount=0;
 function main(canvas,ctx){
     // console.log("tick",count);
     count++;
     if(!stop){
-        stop = count > 1000?true:false;
+        // stop = count > 1000?true:false;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         // generate_divergence(ctx, canvas.width/,600,count, 600);
         var i;
@@ -15,10 +16,15 @@ function main(canvas,ctx){
         // for(i =count;i>0;i--){
         //     generate_divergence(ctx, i,1*i);
         // }}
-        for(i=0; i< count ;i++){
-            generate_divergence(ctx,canvas.width/2,canvas.height/2, count -i , (canvas.height/2 - radiusInc*i));
+
+        for(i=removeCount; i< count ;i++){
+            generate_divergence(ctx,canvas.width/2,canvas.height/2, i , (radiusInc*(count-i)) );
         }
-        
+        if((radiusInc*(count))>canvas.height/2){
+            // if the radius of largest is greater than what can be displayed
+            //remove that from the display que
+            removeCount += 1;
+        }
     }else{
         window.clearInterval();
     }
@@ -58,7 +64,7 @@ function generate_divergence(ctx,center_x,center_y,count,radius){
         // console.log("?", (count >> i) & 1);
         if((count >> i) & 1 == 1){
             // console.log("yes");
-            ctx.fillStyle = "";
+            ctx.fillStyle = "white";
         }else{
             // console.log("no");
             ctx.fillStyle = "black";
